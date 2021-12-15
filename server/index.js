3df853892bc9a3a15fdac4c8562535d3078615b2
Wyatt Enourato <wenourato@gmail.com>
2021-12-15 16:38:51 -0700

    update final?

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../style.css'))
+  })
+
 const port = process.env.PORT || 4005
 
 app.listen(port, () => {console.log(`Dad?! are you there?! ... on port ${port}.`)})
